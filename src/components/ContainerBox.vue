013def039957b0cd44f2b517f0a252efa56d7dd6
gminnimk <tvt1245@gmail.com>
2025-03-11 14:14:00 +0900

    feat: 기본 템플릿 추가

---
+++ src/components/ContainerBox.vue
@@ -0,0 +1,15 @@
+<script setup>
+import PostComponent from "@/components/PostComponent.vue";
+</script>
+
+<template>
+  <div>
+    <PostComponent/>
+    <PostComponent/>
+    <PostComponent/>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
