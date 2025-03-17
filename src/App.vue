3f2caf0198e192448aab14e92ccaf80e4127b93a
gminnimk <tvt1245@gmail.com>
2025-03-17 11:41:31 +0900

    feat: 기본 초기 세팅(데이터, 컨테이너 , props 설정..)

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,117 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <div class="header">
+    <ul class="header-button-left">
+      <li>Cancel</li>
+    </ul>
+    <ul class="header-button-right">
+      <li>Next</li>
+    </ul>
+    <img src="./assets/logo.png" class="logo"/>
+  </div>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
+  <ContainerBox
+      :postData="postData"
+  />
 
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="footer">
+    <ul class="footer-button-plus">
+      <input type="file" id="file" class="inputfile"/>
+      <label for="file" class="input-plus">+</label>
+    </ul>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
+<script setup>
+import postData from './assets/postData'
+import ContainerBox from "@/components/ContainerBox.vue";
+</script>
+
+<style>
+body {
+  margin: 0;
+}
+
+ul {
+  padding: 5px;
+  list-style-type: none;
 }
 
 .logo {
+  width: 22px;
+  margin: auto;
   display: block;
-  margin: 0 auto 2rem;
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 13px;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+.header {
+  width: 100%;
+  height: 40px;
+  background-color: white;
+  padding-bottom: 8px;
+  position: sticky;
+  top: 0;
+}
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+.header-button-left {
+  color: skyblue;
+  float: left;
+  width: 50px;
+  padding-left: 20px;
+  cursor: pointer;
+  margin-top: 10px;
+}
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.header-button-right {
+  color: skyblue;
+  float: right;
+  width: 50px;
+  cursor: pointer;
+  margin-top: 10px;
+}
+
+.footer {
+  width: 100%;
+  position: sticky;
+  bottom: 0;
+  padding-bottom: 10px;
+  background-color: white;
+}
+
+.footer-button-plus {
+  width: 80px;
+  margin: auto;
+  text-align: center;
+  cursor: pointer;
+  font-size: 24px;
+  padding-top: 12px;
+}
+
+.sample-box {
+  width: 100%;
+  height: 600px;
+  background-color: bisque;
+}
+
+.inputfile {
+  display: none;
+}
+
+.input-plus {
+  cursor: pointer;
+}
+
+#app {
+  box-sizing: border-box;
+  font-family: "consolas";
+  margin-top: 60px;
+  width: 100%;
+  max-width: 460px;
+  margin: auto;
+  position: relative;
+  border-right: 1px solid #eee;
+  border-left: 1px solid #eee;
 }
 </style>
---
+++ src/components/ContainerBox.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <PostBox
+        :postData="postData"
+    />
+  </div>
+</template>
+
+<script setup>
+import PostBox from "./PostBox.vue"
+import {defineProps} from "vue"
+
+defineProps({
+  postData: ({
+    type: Array
+  })
+})
+</script>
+
+<style scoped>
+
+</style>
