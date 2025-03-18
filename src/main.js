c47b630b8f5ff9e98800f0d390c147f96e065f1a
gminnimk <tvt1245@gmail.com>
2025-03-18 10:00:46 +0900

    feat: pinia 초기 설정

--- src/main.js
+++ src/main.js
@@ -2,10 +2,13 @@ import {createApp} from 'vue'
 import App from './App.vue'
 import axios from 'axios'
 import mitt from 'mitt'
+import {createPinia} from 'pinia'
 
-const app = createApp(App)
 const emitter = mitt()
+const pinia = createPinia()
+const app = createApp(App)
 app.config.globalProperties.axios = axios
 app.config.globalProperties.emitter = emitter
 app.provide('emitter', emitter)
+app.use(pinia)
 app.mount('#app')
